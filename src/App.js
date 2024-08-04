5ad91ebb03efe984782a232d02fac7342e7fb124
dcolin798@gmail.com <dcolin798@gmail.com>
2024-08-04 10:30:19 -0600

    inicio de parte 1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () =>{
   return (
     <div className="App">
       <Mensaje messaje='hola gente como estan '/>
+      <Mensaje messaje='yo etoy aprendiendo un poco de react'/>
       <Description/>
     </div>
   );
